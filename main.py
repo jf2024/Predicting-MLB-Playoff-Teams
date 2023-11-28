eece23dd6d8742764caa15a8d5018c08f3fe5d6b
Jose Xavier Fuentes <josexfuentes27@gmail.com>
2023-11-28 14:01:25 -0800

    added more models and accuracy, foundation to build from

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC 
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 
+# Read the dataset
 df = pd.read_csv('baseball.csv')
 
 # Histogram of runs scored 
@@ -22,14 +26,44 @@ plt.ylabel('Runs Against')
 plt.show()
 
 # Features and Targets
-X = df[['RS','RA','OBP','SLG']] 
+X = df[['RS', 'RA', 'OBP', 'SLG']] 
 y = df['Playoffs']
 
-# Train
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-model = LogisticRegression()
-model.fit(X_train, y_train)
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 
-# Test
-y_pred = model.predict(X_test)
-print("Accuracy:", accuracy_score(y_test, y_pred))
+# Print the number of samples in the training and testing datasets
+print("Number of samples in training set:", X_train.shape[0])
+print("Number of samples in testing set:", X_test.shape[0])
+
+# Logistic Regression
+log_reg = LogisticRegression()
+log_reg.fit(X_train, y_train)
+train_score_log_reg = log_reg.score(X_train, y_train)
+test_score_log_reg = log_reg.score(X_test, y_test)
+print("Logistic Regression Training Accuracy:", train_score_log_reg)
+print("Logistic Regression Test Accuracy:", test_score_log_reg)
+
+# Random Forest
+rf = RandomForestClassifier(n_estimators=100)
+rf.fit(X_train, y_train) 
+train_score_rf = rf.score(X_train, y_train)
+test_score_rf = rf.score(X_test, y_test)
+print("Random Forest Training Accuracy:", train_score_rf)
+print("Random Forest Test Accuracy:", test_score_rf)
+
+# SVM
+svm = SVC()
+svm.fit(X_train, y_train)
+train_score_svm = svm.score(X_train, y_train)
+test_score_svm = svm.score(X_test, y_test)
+print("SVM Training Accuracy:", train_score_svm)
+print("SVM Test Accuracy:", test_score_svm)
+
+# KNN
+knn = KNeighborsClassifier()  
+knn.fit(X_train, y_train)
+train_score_knn = knn.score(X_train, y_train)
+test_score_knn = knn.score(X_test, y_test)
+print("KNN Training Accuracy:", train_score_knn)
+print("KNN Test Accuracy:", test_score_knn)
