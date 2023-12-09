eb7450e5b4760f0f1dc9ddd3db4b3b9e114dae08
Jose Xavier Fuentes <josexfuentes27@gmail.com>
2023-12-09 14:01:46 -0800

    added actual graphs for playoffs vs our models predicted graphs for playoffs

--- moneyball.py
+++ moneyball.py
@@ -1,3 +1,7 @@
+# this will be our final submission for the project
+# just need to write descriptions for each of the functions 
+#change name later 
+
 import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.linear_model import LogisticRegression
@@ -6,6 +10,8 @@ from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.impute import SimpleImputer
 from sklearn.metrics import accuracy_score, classification_report
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def read_dataset(file_path):
     return pd.read_csv(file_path)
@@ -52,16 +58,52 @@ def ablative_analysis(model, features, X_train, X_test, y_train, y_test):
 
     return scores
 
+def plot_playoff_counts(actual_playoff_counts, predicted_playoff_counts, model_name):
+    plt.figure(figsize=(12, 6))
+
+    # Actual Playoff Appearances
+    plt.subplot(1, 2, 1)
+    sns.barplot(x='Actual_Playoff_Appearances', y='Team', data=actual_playoff_counts.sort_values(by='Actual_Playoff_Appearances', ascending=False))
+    plt.title(f'{model_name} Actual Playoff Appearances')
+
+    # Predicted Playoff Appearances
+    plt.subplot(1, 2, 2)
+    sns.barplot(x='Predicted_Playoff_Appearances', y='Team', data=predicted_playoff_counts.sort_values(by='Predicted_Playoff_Appearances', ascending=False))
+    plt.title(f'{model_name} Predicted Playoff Appearances')
+
+    plt.tight_layout()
+    plt.show()
+
 def main():
     file_path = 'baseball.csv'
     df = read_dataset(file_path)
 
     X = df[['RS', 'RA', 'OBP', 'SLG', 'OOBP', 'OSLG']]
     y = df['Playoffs']
+    
+
+    # Check for missing values in the dataset
+    missing_values = X.isnull().sum()
+
+    # Display columns with missing values, if any
+    print("Columns with missing values:")
+    print(missing_values[missing_values > 0])
+
+	# Impute missing values only for columns with missing values
+    cols_with_missing_values = missing_values[missing_values > 0].index
+
+	# Exclude 'RankSeason' and 'RankPlayoffs' from the imputation
+    cols_to_impute = [col for col in cols_with_missing_values if col not in ['RankSeason', 'RankPlayoffs']]
+    imputer = SimpleImputer(strategy='mean')
+    X[cols_to_impute] = imputer.fit_transform(X[cols_to_impute])
 
     X_train, X_test, y_train, y_test = split_data(X, y)
 
     X_train_imputed, X_test_imputed = impute_missing_values(X_train, X_test)
+
+    # Print the number of samples in the training and testing datasets
+    print("Number of samples in training set:", X_train.shape[0])
+    print("Number of samples in testing set:", X_test.shape[0])
 
     models = {
         'Logistic Regression': LogisticRegression(),
@@ -77,6 +119,36 @@ def main():
         print(f"{model_name} Test Accuracy: {test_score}")
         print(f"{model_name} Classification Report:\n", classification_report(y_test, y_pred))
 
+        # Add predictions to the original DataFrame
+        df['Predicted_Playoffs'] = model.predict(X)
+
+        # Get playoff counts based on the actual dataset
+        actual_playoff_counts = df.groupby("Team")["Playoffs"].sum().reset_index(name="Actual_Playoff_Appearances")
+
+        # Get playoff counts based on the model predictions
+        predicted_playoff_counts = df.groupby("Team")["Predicted_Playoffs"].sum().reset_index(name="Predicted_Playoff_Appearances")
+
+        # Print counts
+        print("\nActual Playoff Appearances:")
+        print(actual_playoff_counts)
+
+        print("\nPredicted Playoff Appearances (Default):")
+        print(predicted_playoff_counts)
+
+        # Plot playoff counts
+        plot_playoff_counts(actual_playoff_counts, predicted_playoff_counts, model_name)
+
+        if model_name == 'Random Forest':
+            feature_importances = model.feature_importances_
+            features = X.columns
+            importance_df = pd.DataFrame({'Feature': features, 'Importance': feature_importances})
+            importance_df = importance_df.sort_values(by='Importance', ascending=False)
+        
+            plt.figure(figsize=(10, 6))
+            sns.barplot(x='Importance', y='Feature', data=importance_df)
+            plt.title(f'{model_name} Feature Importance')
+            plt.show() 
+
     for model_name, model in models.items():
         print("\n------------------------------------------------------------------------------")
         print(f"\n{model_name} Hyperparameter Tuning:")
@@ -86,6 +158,25 @@ def main():
         print(f"{model_name} Training Accuracy (Hyperparameter Tuning): {best_score}")
         print(f"{model_name} Test Accuracy (Hyperparameter Tuning): {test_score}")
         print(f"{model_name} Classification Report:\n", classification_report(y_test, y_pred))
+
+        # Add predictions to the original DataFrame
+        df['Predicted_Playoffs'] = model.predict(X)
+
+        # Get playoff counts based on the actual dataset
+        actual_playoff_counts = df.groupby("Team")["Playoffs"].sum().reset_index(name="Actual_Playoff_Appearances")
+
+        # Get playoff counts based on the model predictions
+        predicted_playoff_counts = df.groupby("Team")["Predicted_Playoffs"].sum().reset_index(name="Predicted_Playoff_Appearances")
+
+        # Print counts
+        print("\nActual Playoff Appearances:")
+        print(actual_playoff_counts)
+
+        print("\nPredicted Playoff Appearances (Hypertuning):")
+        print(predicted_playoff_counts)
+
+        # Plot playoff counts
+        plot_playoff_counts(actual_playoff_counts, predicted_playoff_counts, model_name)
 
     print("\nAblative Analysis Results (No Hyperparameters):")
     for model_name, model in models.items():
@@ -113,3 +204,4 @@ def get_hyperparameter_grid(model_name):
 
 if __name__ == "__main__":
     main()
+
